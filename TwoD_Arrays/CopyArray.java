7349fd90b463b384a182e2e0a10a668da94ad138
shiva krishna <shivakrishna.d16@gmail.com>
2024-05-07 06:12:37 +0530

    Intial Commit
    Signed-off-by: shiva krishna <shivakrishna.d16@gmail.com>

---
+++ TwoD_Arrays/CopyArray.java
@@ -0,0 +1,15 @@
+package TwoD_Arrays;
+
+import java.util.Arrays;
+
+public class CopyArray {
+    public static void main(String[] args) {
+        int[] A = new int[]{10, 20, 30, 40, 50};
+        int B = 5;
+        int[] c = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            c[i] = A[i] + B;
+        }
+        System.out.println("new Array::" + Arrays.toString(c));
+    }
+}
