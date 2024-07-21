39bcab37ff3fc5bf57b510238d76c17d97d2591b
shiva krishna <shivakrishna.d16@gmail.com>
2024-07-21 06:31:43 +0530

    OOPS
    Signed-off-by: shiva krishna <shivakrishna.d16@gmail.com>

---
+++ June2024/methodsPractice/MainClass2.java
@@ -0,0 +1,10 @@
+package June2024.methodsPractice;
+
+
+import June2024.oops.Employee;
+
+public class MainClass2 {
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+    }
+}
---
+++ June2024/oops/Inherintence/Father.java
@@ -0,0 +1,54 @@
+package June2024.oops.Inherintence;
+
+public class Father {
+    private String firstName;
+    private String lastName;
+    private String lands;
+    private String money;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLands() {
+        return lands;
+    }
+
+    public void setLands(String lands) {
+        this.lands = lands;
+    }
+
+    public String getMoney() {
+        return money;
+    }
+
+    public void setMoney(String money) {
+        this.money = money;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void displayInfo() {
+        System.out.println("Hiiiiiiiiiiii");
+    }
+
+}
