70715175992e068cbbd3a456fda43b54ef1d2a07
shiva krishna <shivakrishna.d16@gmail.com>
2024-07-23 06:21:57 +0530

    OOPS
    Signed-off-by: shiva krishna <shivakrishna.d16@gmail.com>

--- June2024/oops/MethodOverriding.java
+++ June2024/oops/MethodOverriding.java
@@ -2,24 +2,24 @@ package June2024.oops;
 
 
 class A {
-
     int addition(int a, int b) {
-
         return a + b;
     }
-
 }
 
-public class MethodOverriding extends A {
-
-
-    public static void main(String[] args) {
-
-    }
+class B extends A {
 
     @Override
     int addition(int a, int b) {
         return a + b;
     }
+}
+
+public class MethodOverriding {
+    public static void main(String[] args) {
+        B b = new B();
+        System.out.println(b.addition(10, 20));
+    }
+
 
 }
